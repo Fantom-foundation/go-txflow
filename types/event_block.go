b2c8b174bb81ed4fed3b62a023882e74620d8faf
Andre Cronje <andre@fantom.foundation>
2019-06-21 17:46:28 +0200

    Test coverage for event block
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

--- types/event_block.go
+++ types/event_block.go
@@ -10,15 +10,14 @@ import (
 
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/merkle"
-	"github.com/tendermint/tendermint/crypto/tmhash"
 	cmn "github.com/tendermint/tendermint/libs/common"
-	"github.com/tendermint/tendermint/version"
 	"github.com/tendermint/tendermint/types"
+	"github.com/tendermint/tendermint/version"
 )
 
 const (
 	// MaxHeaderBytes is a maximum header size (including amino overhead).
-	MaxHeaderBytes int64 = 653
+	MaxHeaderBytes int64 = 550
 
 	// MaxAminoOverheadForBlock - maximum amino overhead to encode a block (up to
 	// MaxBlockSizeBytes in size) not including it's parts except Data.
@@ -34,18 +33,18 @@ const (
 
 // EventBlock defines the atomic unit of a aBFT blockchain.
 type EventBlock struct {
-	mtx      sync.Mutex
-	Header   `json:"header"`
-	types.Data     `json:"data"`
-	Evidence types.EvidenceData `json:"evidence"`
-	SelfEventBlockID EventBlockID `json:"self_event_block_id"`
-	EventBlockIDs    []EventBlockID      `json:"block_ids"`
+	mtx              sync.Mutex
+	Header           `json:"header"`
+	types.Data       `json:"data"`
+	Evidence         types.EvidenceData `json:"evidence"`
+	SelfEventBlockID types.BlockID      `json:"self_event_block_id"`
+	EventBlockIDs    []types.BlockID    `json:"block_ids"`
 }
 
 // MakeEventBlock returns a new event block with an empty header, except what can be
 // computed from itself.
 // It populates the same set of fields validated by ValidateBasic.
-func MakeEventBlock(height int64, txs []types.Tx, evidence []types.Evidence, eventBlockIDs []EventBlockID) *EventBlock {
+func MakeEventBlock(height int64, txs []types.Tx, evidence []types.Evidence, eventBlockIDs []types.BlockID) *EventBlock {
 	block := &EventBlock{
 		Header: Header{
 			Height: height,
@@ -54,7 +53,7 @@ func MakeEventBlock(height int64, txs []types.Tx, evidence []types.Evidence, eve
 		Data: types.Data{
 			Txs: txs,
 		},
-		Evidence: types.EvidenceData{Evidence: evidence},
+		Evidence:      types.EvidenceData{Evidence: evidence},
 		EventBlockIDs: eventBlockIDs,
 	}
 	block.fillHeader()
@@ -98,81 +97,58 @@ func (eb *EventBlock) ValidateBasic() error {
 	if eb.TotalTxs < 0 {
 		return errors.New("Negative Header.TotalTxs")
 	}
-
-	if err := eb.LastBlockID.ValidateBasic(); err != nil {
-		return fmt.Errorf("Wrong Header.LastBlockID: %v", err)
-	}
-
-	// Validate the last commit and its hash.
-	// Validate the hashes of the parents, not the commit of the previous block
-	if eb.Header.Height > 1 {
-		if eb.LastCommit == nil {
-			return errors.New("nil LastCommit")
+	for _, lastBlockID := range eb.LastEventBlockIDs {
+		if err := lastBlockID.ValidateBasic(); err != nil {
+			return fmt.Errorf("Wrong Header.LastBlockID: %v", err)
 		}
-		if err := eb.LastCommit.ValidateBasic(); err != nil {
-			return fmt.Errorf("Wrong LastCommit")
-		}
-	}
-	if err := ValidateHash(eb.LastCommitHash); err != nil {
-		return fmt.Errorf("Wrong Header.LastCommitHash: %v", err)
-	}
-	if !bytes.Equal(eb.LastCommitHash, eb.LastCommit.Hash()) {
-		return fmt.Errorf("Wrong Header.LastCommitHash. Expected %v, got %v",
-			eb.LastCommit.Hash(),
-			eb.LastCommitHash,
-		)
 	}
 
 	// Validate the hash of the transactions.
 	// NOTE: b.Data.Txs may be nil, but b.Data.Hash()
 	// still works fine
-	if err := ValidateHash(b.DataHash); err != nil {
+	if err := types.ValidateHash(eb.DataHash); err != nil {
 		return fmt.Errorf("Wrong Header.DataHash: %v", err)
 	}
-	if !bytes.Equal(b.DataHash, b.Data.Hash()) {
+	if !bytes.Equal(eb.DataHash, eb.Data.Hash()) {
 		return fmt.Errorf(
 			"Wrong Header.DataHash. Expected %v, got %v",
-			b.Data.Hash(),
-			b.DataHash,
+			eb.Data.Hash(),
+			eb.DataHash,
 		)
 	}
 
 	// Basic validation of hashes related to application data.
 	// Will validate fully against state in state#ValidateBlock.
-	if err := ValidateHash(b.ValidatorsHash); err != nil {
+	if err := types.ValidateHash(eb.ValidatorsHash); err != nil {
 		return fmt.Errorf("Wrong Header.ValidatorsHash: %v", err)
 	}
-	if err := ValidateHash(b.NextValidatorsHash); err != nil {
+	if err := types.ValidateHash(eb.NextValidatorsHash); err != nil {
 		return fmt.Errorf("Wrong Header.NextValidatorsHash: %v", err)
 	}
-	if err := ValidateHash(b.ConsensusHash); err != nil {
+	if err := types.ValidateHash(eb.ConsensusHash); err != nil {
 		return fmt.Errorf("Wrong Header.ConsensusHash: %v", err)
-	}
-	// NOTE: AppHash is arbitrary length
-	if err := ValidateHash(b.LastResultsHash); err != nil {
-		return fmt.Errorf("Wrong Header.LastResultsHash: %v", err)
 	}
 
 	// Validate evidence and its hash.
-	if err := ValidateHash(b.EvidenceHash); err != nil {
+	if err := types.ValidateHash(eb.EvidenceHash); err != nil {
 		return fmt.Errorf("Wrong Header.EvidenceHash: %v", err)
 	}
 	// NOTE: b.Evidence.Evidence may be nil, but we're just looping.
-	for i, ev := range b.Evidence.Evidence {
+	for i, ev := range eb.Evidence.Evidence {
 		if err := ev.ValidateBasic(); err != nil {
 			return fmt.Errorf("Invalid evidence (#%d): %v", i, err)
 		}
 	}
-	if !bytes.Equal(b.EvidenceHash, b.Evidence.Hash()) {
+	if !bytes.Equal(eb.EvidenceHash, eb.Evidence.Hash()) {
 		return fmt.Errorf("Wrong Header.EvidenceHash. Expected %v, got %v",
-			b.EvidenceHash,
-			b.Evidence.Hash(),
+			eb.EvidenceHash,
+			eb.Evidence.Hash(),
 		)
 	}
 
-	if len(b.ProposerAddress) != crypto.AddressSize {
+	if len(eb.ProposerAddress) != crypto.AddressSize {
 		return fmt.Errorf("Expected len(Header.ProposerAddress) to be %d, got %d",
-			crypto.AddressSize, len(b.ProposerAddress))
+			crypto.AddressSize, len(eb.ProposerAddress))
 	}
 
 	return nil
@@ -220,7 +196,7 @@ func (eb *EventBlock) MakePartSet(partSize int) *types.PartSet {
 	if err != nil {
 		panic(err)
 	}
-	return NewPartSetFromData(bz, partSize)
+	return types.NewPartSetFromData(bz, partSize)
 }
 
 // HashesTo is a convenience function that checks if a block hashes to the given argument.
@@ -304,8 +280,8 @@ func MaxDataBytes(maxBytes int64, valsCount, evidenceCount int) int64 {
 	maxDataBytes := maxBytes -
 		MaxAminoOverheadForBlock -
 		MaxHeaderBytes -
-		int64(valsCount)*MaxVoteBytes -
-		int64(evidenceCount)*MaxEvidenceBytes
+		int64(valsCount)*types.MaxVoteBytes -
+		int64(evidenceCount)*types.MaxEvidenceBytes
 
 	if maxDataBytes < 0 {
 		panic(fmt.Sprintf(
@@ -325,11 +301,10 @@ func MaxDataBytes(maxBytes int64, valsCount, evidenceCount int) int64 {
 //
 // XXX: Panics on negative result.
 func MaxDataBytesUnknownEvidence(maxBytes int64, valsCount int) int64 {
-	_, maxEvidenceBytes := MaxEvidencePerBlock(maxBytes)
+	_, maxEvidenceBytes := types.MaxEvidencePerBlock(maxBytes)
 	maxDataBytes := maxBytes -
 		MaxAminoOverheadForBlock -
 		MaxHeaderBytes -
-		int64(valsCount)*MaxVoteBytes -
 		maxEvidenceBytes
 
 	if maxDataBytes < 0 {
@@ -360,43 +335,41 @@ type Header struct {
 	TotalTxs int64             `json:"total_txs"`
 
 	// self prev block info
-	SelfLastEventBlockID EventBlockID `json:"self_last_block_id"`
+	SelfLastEventBlockID types.BlockID `json:"self_last_block_id"`
 
 	// prev block info
-	LastEventBlockIDs []EventBlockID `json:"last_block_ids"`
+	LastEventBlockIDs []types.BlockID `json:"last_block_ids"`
 
 	// hashes of block data
-	DataHash       cmn.HexBytes `json:"data_hash"`        // transactions
+	DataHash cmn.HexBytes `json:"data_hash"` // transactions
 
 	// hashes from the app output from the prev block
 	ValidatorsHash     cmn.HexBytes `json:"validators_hash"`      // validators for the current block
 	NextValidatorsHash cmn.HexBytes `json:"next_validators_hash"` // validators for the next block
 	ConsensusHash      cmn.HexBytes `json:"consensus_hash"`       // consensus params for current block
-	LastResultsHash    cmn.HexBytes `json:"last_results_hash"`    // root hash of all results from the txs from the previous block
 
 	// consensus info
-	EvidenceHash    cmn.HexBytes `json:"evidence_hash"`    // evidence included in the block
-	ProposerAddress types.Address      `json:"proposer_address"` // original proposer of the block
+	EvidenceHash    cmn.HexBytes  `json:"evidence_hash"`    // evidence included in the block
+	ProposerAddress types.Address `json:"proposer_address"` // original proposer of the block
 }
 
 // Populate the Header with state-derived data.
 // Call this after MakeBlock to complete the Header.
 func (h *Header) Populate(
 	version version.Consensus, chainID string,
-	timestamp time.Time, lastBlockIDs []EventBlockID, totalTxs int64,
+	timestamp time.Time, lastEventBlockIDs []types.BlockID, totalTxs int64,
 	valHash, nextValHash []byte,
 	consensusHash, lastResultsHash []byte,
-	proposerAddress Address,
+	proposerAddress types.Address,
 ) {
 	h.Version = version
 	h.ChainID = chainID
 	h.Time = timestamp
-	h.LastBlockIDs = lastBlockIDs
+	h.LastEventBlockIDs = lastEventBlockIDs
 	h.TotalTxs = totalTxs
 	h.ValidatorsHash = valHash
 	h.NextValidatorsHash = nextValHash
 	h.ConsensusHash = consensusHash
-	h.LastResultsHash = lastResultsHash
 	h.ProposerAddress = proposerAddress
 }
 
@@ -417,14 +390,12 @@ func (h *Header) Hash() cmn.HexBytes {
 		cdcEncode(h.Time),
 		cdcEncode(h.NumTxs),
 		cdcEncode(h.TotalTxs),
-		cdcEncode(h.SelfLastBlockID),
-		cdcEncode(h.LastBlockIDs),
-		cdcEncode(h.LastCommitHash),
+		cdcEncode(h.SelfLastEventBlockID),
+		cdcEncode(h.LastEventBlockIDs),
 		cdcEncode(h.DataHash),
 		cdcEncode(h.ValidatorsHash),
 		cdcEncode(h.NextValidatorsHash),
 		cdcEncode(h.ConsensusHash),
-		cdcEncode(h.LastResultsHash),
 		cdcEncode(h.EvidenceHash),
 		cdcEncode(h.ProposerAddress),
 	})
@@ -442,12 +413,11 @@ func (h *Header) StringIndented(indent string) string {
 %s  Time:           %v
 %s  NumTxs:         %v
 %s  TotalTxs:       %v
-%s  LastBlockID:    %v
+%s  LastEventBlockIDs:    %v
 %s  Data:           %v
 %s  Validators:     %v
 %s  NextValidators: %v
 %s  Consensus:      %v
-%s  Results:        %v
 %s  Evidence:       %v
 %s  Proposer:       %v
 %s}#%v`,
@@ -457,65 +427,12 @@ func (h *Header) StringIndented(indent string) string {
 		indent, h.Time,
 		indent, h.NumTxs,
 		indent, h.TotalTxs,
-		indent, h.LastBlockID,
+		indent, h.LastEventBlockIDs,
 		indent, h.DataHash,
 		indent, h.ValidatorsHash,
 		indent, h.NextValidatorsHash,
 		indent, h.ConsensusHash,
-		indent, h.LastResultsHash,
 		indent, h.EvidenceHash,
 		indent, h.ProposerAddress,
 		indent, h.Hash())
-}
-//--------------------------------------------------------------------------------
-
-// EventBlockID defines the unique ID of a block as its Hash and its PartSetHeader
-type EventBlockID struct {
-	Hash        cmn.HexBytes  `json:"hash"`
-	PartsHeader types.PartSetHeader `json:"parts"`
-}
-
-// Equals returns true if the EventBlockID matches the given EventBlockID
-func (ebID EventBlockID) Equals(other BlockID) bool {
-	return bytes.Equal(ebID.Hash, other.Hash) &&
-		ebID.PartsHeader.Equals(other.PartsHeader)
-}
-
-// Key returns a machine-readable string representation of the BlockID
-func (ebID EventBlockID) Key() string {
-	bz, err := cdc.MarshalBinaryBare(ebID.PartsHeader)
-	if err != nil {
-		panic(err)
-	}
-	return string(ebID.Hash) + string(bz)
-}
-
-// ValidateBasic performs basic validation.
-func (ebID EventBlockID) ValidateBasic() error {
-	// Hash can be empty in case of POLBlockID in Proposal.
-	if err := ValidateHash(ebID.Hash); err != nil {
-		return fmt.Errorf("Wrong Hash")
-	}
-	if err := ebID.PartsHeader.ValidateBasic(); err != nil {
-		return fmt.Errorf("Wrong PartsHeader: %v", err)
-	}
-	return nil
-}
-
-// IsZero returns true if this is the BlockID of a nil block.
-func (ebID EventBlockID) IsZero() bool {
-	return len(ebID.Hash) == 0 &&
-		ebID.PartsHeader.IsZero()
-}
-
-// IsComplete returns true if this is a valid BlockID of a non-nil block.
-func (ebID EventBlockID) IsComplete() bool {
-	return len(ebID.Hash) == tmhash.Size &&
-		ebID.PartsHeader.Total > 0 &&
-		len(ebID.PartsHeader.Hash) == tmhash.Size
-}
-
-// String returns a human readable string representation of the EventBlockID
-func (ebID EventBlockID) String() string {
-	return fmt.Sprintf(`%v:%v`, ebID.Hash, ebID.PartsHeader)
 }
---
+++ types/event_block_test.go
@@ -0,0 +1,224 @@
+package types
+
+import (
+	// it is ok to use math/rand here: we do not need a cryptographically secure random
+	// number generator here and we can run the tests a bit faster
+	"crypto/rand"
+	"math"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/tmhash"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	"github.com/tendermint/tendermint/types"
+	"github.com/tendermint/tendermint/version"
+)
+
+func TestMain(m *testing.M) {
+	types.RegisterMockEvidences(cdc)
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestEventBlockAddEvidence(t *testing.T) {
+	txs := []types.Tx{types.Tx("foo"), types.Tx("bar")}
+	lastID := makeBlockIDRandom()
+	h := int64(3)
+
+	_, valSet, _ := randVoteSet(h-1, 1, types.PrecommitType, 10, 1)
+
+	ev := types.NewMockGoodEvidence(h, 0, valSet.Validators[0].Address)
+	evList := []types.Evidence{ev}
+
+	block := MakeEventBlock(h, txs, evList, []types.BlockID{lastID})
+	require.NotNil(t, block)
+	require.Equal(t, 1, len(block.Evidence.Evidence))
+	require.NotNil(t, block.EvidenceHash)
+}
+
+func TestEventBlockValidateBasic(t *testing.T) {
+	require.Error(t, (*EventBlock)(nil).ValidateBasic())
+
+	txs := []types.Tx{types.Tx("foo"), types.Tx("bar")}
+	lastID := makeBlockIDRandom()
+	h := int64(3)
+
+	_, valSet, _ := randVoteSet(h-1, 1, types.PrecommitType, 10, 1)
+
+	ev := types.NewMockGoodEvidence(h, 0, valSet.Validators[0].Address)
+	evList := []types.Evidence{ev}
+
+	testCases := []struct {
+		testName      string
+		malleateBlock func(*EventBlock)
+		expErr        bool
+	}{
+		{"Make Block", func(blk *EventBlock) {}, false},
+		{"Make Block w/ proposer Addr", func(blk *EventBlock) { blk.ProposerAddress = valSet.GetProposer().Address }, false},
+		{"Negative Height", func(blk *EventBlock) { blk.Height = -1 }, true},
+		{"Increase NumTxs", func(blk *EventBlock) { blk.NumTxs++ }, true},
+		{"Tampered Data", func(blk *EventBlock) {
+			blk.Data.Txs[0] = types.Tx("something else")
+			blk.DataHash = cmn.RandBytes(len(blk.DataHash))
+		}, true},
+		{"Tampered DataHash", func(blk *EventBlock) {
+			blk.DataHash = cmn.RandBytes(len(blk.DataHash))
+		}, true},
+		{"Tampered EvidenceHash", func(blk *EventBlock) {
+			blk.EvidenceHash = []byte("something else")
+		}, true},
+	}
+	for i, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			block := MakeEventBlock(h, txs, evList, []types.BlockID{lastID})
+			block.ProposerAddress = valSet.GetProposer().Address
+			tc.malleateBlock(block)
+			err := block.ValidateBasic()
+			assert.Equal(t, tc.expErr, err != nil, "#%d: %v", i, err)
+		})
+	}
+}
+
+func TestEventBlockHash(t *testing.T) {
+	assert.Nil(t, (*EventBlock)(nil).Hash())
+	assert.Nil(t, MakeEventBlock(int64(3), []types.Tx{types.Tx("Hello World")}, nil, nil).Hash())
+}
+
+func TestEventBlockMakePartSet(t *testing.T) {
+	assert.Nil(t, (*EventBlock)(nil).MakePartSet(2))
+
+	partSet := MakeEventBlock(int64(3), []types.Tx{types.Tx("Hello World")}, nil, nil).MakePartSet(1024)
+	assert.NotNil(t, partSet)
+	assert.Equal(t, 1, partSet.Total())
+}
+
+func TestEventBlockMakePartSetWithEvidence(t *testing.T) {
+	assert.Nil(t, (*EventBlock)(nil).MakePartSet(2))
+
+	lastID := makeBlockIDRandom()
+	h := int64(3)
+
+	_, valSet, _ := randVoteSet(h-1, 1, types.PrecommitType, 10, 1)
+
+	ev := types.NewMockGoodEvidence(h, 0, valSet.Validators[0].Address)
+	evList := []types.Evidence{ev}
+
+	partSet := MakeEventBlock(h, []types.Tx{types.Tx("Hello World")}, evList, []types.BlockID{lastID}).MakePartSet(1024)
+	assert.NotNil(t, partSet)
+	assert.Equal(t, 1, partSet.Total())
+}
+
+func TestEventBlockHashesTo(t *testing.T) {
+	assert.False(t, (*EventBlock)(nil).HashesTo(nil))
+
+	lastID := makeBlockIDRandom()
+	h := int64(3)
+	_, valSet, _ := randVoteSet(h-1, 1, types.PrecommitType, 10, 1)
+
+	ev := types.NewMockGoodEvidence(h, 0, valSet.Validators[0].Address)
+	evList := []types.Evidence{ev}
+
+	block := MakeEventBlock(h, []types.Tx{types.Tx("Hello World")}, evList, []types.BlockID{lastID})
+	block.ValidatorsHash = valSet.Hash()
+	assert.False(t, block.HashesTo([]byte{}))
+	assert.False(t, block.HashesTo([]byte("something else")))
+	assert.True(t, block.HashesTo(block.Hash()))
+}
+
+func TestEventBlockSize(t *testing.T) {
+	size := MakeEventBlock(int64(3), []types.Tx{types.Tx("Hello World")}, nil, nil).Size()
+	if size <= 0 {
+		t.Fatal("Size of the block is zero or negative")
+	}
+}
+
+func TestBlockString(t *testing.T) {
+	assert.Equal(t, "nil-Block", (*EventBlock)(nil).String())
+	assert.Equal(t, "nil-Block", (*EventBlock)(nil).StringIndented(""))
+	assert.Equal(t, "nil-Block", (*EventBlock)(nil).StringShort())
+
+	block := MakeEventBlock(int64(3), []types.Tx{types.Tx("Hello World")}, nil, nil)
+	assert.NotEqual(t, "nil-Block", block.String())
+	assert.NotEqual(t, "nil-Block", block.StringIndented(""))
+	assert.NotEqual(t, "nil-Block", block.StringShort())
+}
+
+func makeBlockIDRandom() types.BlockID {
+	blockHash := make([]byte, tmhash.Size)
+	partSetHash := make([]byte, tmhash.Size)
+	rand.Read(blockHash)   //nolint: gosec
+	rand.Read(partSetHash) //nolint: gosec
+	blockPartsHeader := types.PartSetHeader{123, partSetHash}
+	return types.BlockID{blockHash, blockPartsHeader}
+}
+
+func makeBlockID(hash []byte, partSetSize int, partSetHash []byte) types.BlockID {
+	return types.BlockID{
+		Hash: hash,
+		PartsHeader: types.PartSetHeader{
+			Total: partSetSize,
+			Hash:  partSetHash,
+		},
+	}
+
+}
+
+var nilBytes []byte
+
+func TestNilHeaderHashDoesntCrash(t *testing.T) {
+	assert.Equal(t, []byte((*Header)(nil).Hash()), nilBytes)
+	assert.Equal(t, []byte((new(Header)).Hash()), nilBytes)
+}
+
+func TestNilDataHashDoesntCrash(t *testing.T) {
+	assert.Equal(t, []byte((*types.Data)(nil).Hash()), nilBytes)
+	assert.Equal(t, []byte(new(types.Data).Hash()), nilBytes)
+}
+
+func TestMaxHeaderBytes(t *testing.T) {
+	// Construct a UTF-8 string of MaxChainIDLen length using the supplementary
+	// characters.
+	// Each supplementary character takes 4 bytes.
+	// http://www.i18nguy.com/unicode/supplementary-test.html
+	maxChainID := ""
+	for i := 0; i < types.MaxChainIDLen; i++ {
+		maxChainID += "𠜎"
+	}
+
+	// time is varint encoded so need to pick the max.
+	// year int, month Month, day, hour, min, sec, nsec int, loc *Location
+	timestamp := time.Date(math.MaxInt64, 0, 0, 0, 0, 0, math.MaxInt64, time.UTC)
+
+	h := Header{
+		Version:            version.Consensus{Block: math.MaxInt64, App: math.MaxInt64},
+		ChainID:            maxChainID,
+		Height:             math.MaxInt64,
+		Time:               timestamp,
+		NumTxs:             math.MaxInt64,
+		TotalTxs:           math.MaxInt64,
+		LastEventBlockIDs:  []types.BlockID{makeBlockID(make([]byte, tmhash.Size), math.MaxInt64, make([]byte, tmhash.Size))},
+		DataHash:           tmhash.Sum([]byte("data_hash")),
+		ValidatorsHash:     tmhash.Sum([]byte("validators_hash")),
+		NextValidatorsHash: tmhash.Sum([]byte("next_validators_hash")),
+		ConsensusHash:      tmhash.Sum([]byte("consensus_hash")),
+		EvidenceHash:       tmhash.Sum([]byte("evidence_hash")),
+		ProposerAddress:    crypto.AddressHash([]byte("proposer_address")),
+	}
+
+	bz, err := cdc.MarshalBinaryLengthPrefixed(h)
+	require.NoError(t, err)
+
+	assert.EqualValues(t, MaxHeaderBytes, len(bz))
+}
+
+// NOTE: privValidators are in order
+func randVoteSet(height int64, round int, type_ types.SignedMsgType, numValidators int, votingPower int64) (*types.VoteSet, *types.ValidatorSet, []types.PrivValidator) {
+	valSet, privValidators := types.RandValidatorSet(numValidators, votingPower)
+	return types.NewVoteSet("test_chain_id", height, round, type_, valSet), valSet, privValidators
+}
